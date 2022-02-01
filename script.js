ff78c09e0a18204b5b253cd7f28913c6ac1eb6f7
Tarokh Mohammadi <tarokhDevMoh2020@gmail.com>
2022-02-01 11:49:43 +0330

    main commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text"); 
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+    text.innerText = "Breathe In!";
+    container.className = "container grow";
+    setTimeout(() => {
+        text.innerText = "Hold";
+        setTimeout(() => {
+            text.innerText = "Breathe Out!"
+            container.className = "container shrink";
+        }, holdTime)
+    }, breathTime);
+};
+
+setInterval(breatheAnimation, totalTime);
